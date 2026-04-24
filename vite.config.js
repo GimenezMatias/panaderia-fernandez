0a17202165e62427cb032a0a9482266917baf514
GimenezMatias <165623879+GimenezMatias@users.noreply.github.com>
2026-04-24 05:37:27 -0300

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/panaderia-fernandez/',
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     react(),
